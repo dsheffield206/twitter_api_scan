2a1819b5a144e70c13d2014a57a668b1b985046b
David Sheffield <david.sheffield21@gmail.com>
2016-03-04 14:41:18 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### API To Scan Twitter for Keywords
+
+### Twitter API Scan - Summary
++ This is where I will document this project.
---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+mongoose.connect();
+
+var scanRouter = require(__dirname + '/routes/scan_routes');
+app.use('/api/scan', scanRouter);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log('server is running on port ' + port + ' !');
+});
