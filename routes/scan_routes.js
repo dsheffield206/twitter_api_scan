7f2328e79d5ff40680fb1b19da881aec15ec12d1
David Sheffield <david.sheffield21@gmail.com>
2016-03-10 13:28:03 -0800

    updating server.js and scan_routes.js

--- routes/scan_routes.js
+++ routes/scan_routes.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var jsonParser = require('body-parser').json();
 var handleError = require(__dirname + '/../lib/handle_error');
-
 var scanRouter = module.exports = exports = express.Router();
+var Twit = require('twit');
+var T = new Twit({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret
+});
 
 scanRouter.get('/', function(req, res){
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 mongoose.connect();
+require('dotenv').config();
 
 var scanRouter = require(__dirname + '/routes/scan_routes');
 app.use('/api/scan', scanRouter);
